12580e38082345b0f0dad806d4fec685a09fa303
lixianwen <327357283@qq.com>
2023-04-16 13:54:24 +0800

    binary search variant v3

--- search/binary_search.c
+++ search/binary_search.c
@@ -97,3 +97,27 @@ int bs2(int a[], int n, int value) {
 
     return -1;
 }
+
+
+/*
+Find the FIRST element `i` >= specific value in array `a`
+*/
+int bs3(int a[], int n, int value) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = low + ((high - low) >> 1);
+        if (a[mid] >= value) {
+            if (mid == 0 || a[mid-1] < value) {
+                return mid;
+            } else {
+                high = mid - 1;
+            }
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return -1;
+}
