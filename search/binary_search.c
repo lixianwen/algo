bcfaf2885ce649d4e2b7686f11226a716e91c64d
lixianwen <327357283@qq.com>
2023-04-16 09:54:57 +0800

    feat: implement a simplest recursion binary search

--- search/binary_search.c
+++ search/binary_search.c
@@ -19,3 +19,29 @@ int simplest_binary_search(int a[], int n, int value) {
 
     return -1;
 }
+
+
+/*
+递推公式：bs(p...r) = bs(p...q-1) + bs(q+1...r)
+终止条件：p > r
+
+Time complexity: O(logn)
+Space complexity: O(1)
+*/
+int internal_recursion_binary_search(int a[], int p, int r, int value) {
+    if (p > r) return -1;
+
+    int q = p + ((r - p) >> 1);
+    if (a[q] == value) {
+        return q;
+    } else if (a[q] < value) {
+        return internal_recursion_binary_search(a, q+1, r, value);
+    } else {
+        return internal_recursion_binary_search(a, p, q-1, value);
+    }
+}
+
+
+int recursion_binary_search(int a[], int n, int value) {
+    internal_recursion_binary_search(a, 0, n-1, value);
+}
